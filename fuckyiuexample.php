2cb44ff5d21fb450edd1cdb932cea3e4d8253a7d
BuildTools <unconfigured@null.spigotmc.org>
2024-04-25 17:07:25 +0100

    Initial commit i dont know what this is going to do

---
+++ fuckyiuexample.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Page Title</title>
+<link rel="stylesheet" href="thecss.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
+</head>
+<body>
+
+<div class = "sidebar">
+  
+</div>
+
+
+<h1>Website</h1>
+<p>This Is a fucking example For All The Pages.</p>
+
+
+
+
+</body>
+</html> 
---
+++ template.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Page Title</title>
+<link rel="stylesheet" href="thecss.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="initial-scale=1">
+<style>
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+</style>
+</head>
+<body>
+
+<div class = "headerbar">
+  <h1>Website</h1>
+  <p>This Is an example For All The Pages.</p>
+</div>
+
+<div class = "row">
+
+
+  <div class = "column left">
+  <a href="mainpage.php" class = "button">MainPage </a> 
+  <a href="fuckyiuexample.php" class = "button">create2 </a> 
+  <a href="fuckyiuexample.php" class = "button">create3 </a> 
+  </div>
+
+  <div class = "column centre">
+  </div>
+
+  <div class = "column right">
+  </div>
+</div>
+
+</body>
+</html> 
