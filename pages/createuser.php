a35ffe44b15652c937b7fee45311a5ffb3fd12f6
BuildTools <unconfigured@null.spigotmc.org>
2024-04-28 21:17:47 +0100

    wat

--- pages/createuser.php
+++ pages/createuser.php
@@ -19,8 +19,60 @@ include '../assets/headerbar.php';
 
 
 <div class = "login_box">
+<h3>New Employee</h3>
 
+<form action="" method="post">
+    <div class="row gy-4 mt-1">
+        
+
+
+    <form method="post">
+    <div class="form-outline mb-2">
+<form method="post">
+    <div>
+        <label>Username</label>
+        <input type="text" name="Username" required>
+    </div>
+    </br>
+    <div>
+        <label>Email Adress</label>
+        <input type="text" name="email" required>
+    </div>
+    </br>
+    <div>
+        <label>Password</label>
+        <input type="text" name="password" required>
+    </div>
+    </br>
 </div>
 
+<?php
+
+
+    if(array_key_exists('Create_Employee', $_POST)) {
+
+        $username = $_POST["Username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $passwordhashed = password_hash($password);
+
+        if ($verify == true) {
+        $sql = "INSERT INTO employee(first_name,middle_name,last_name,email,telephone,pin_hash,username,password_hash,salt,job_id,department_id) VALUES('$first_name','$middle_name','$last_name','$email','$phone_num','$pin','$username','$password','$salt','$job_id','$dep_id')";
+        
+            if ($conn->query($sql) === TRUE) {
+                echo "New employee created successfully";
+            } 
+            else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        }
+        else {
+            echo "Invalid Input";
+        }
+    }
+
+    ?>
+</body>
 </body>
 </html> 
