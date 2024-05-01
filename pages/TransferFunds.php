4847535acaa503ed5072f1e03c8f91c5f6038966
BuildTools <unconfigured@null.spigotmc.org>
2024-05-01 02:21:56 +0100

    invisible face

--- pages/TransferFunds.php
+++ pages/TransferFunds.php
@@ -71,18 +71,25 @@ include '../functions/redirect.php'
             $accountidsend = $_POST["AccountIdSend"];
             $amount = $_POST["Amount"];
             $password = $_POST["password"];
-
-
+            $sql = "SELECT Currency.Togbp From Account_Table Join Currency ON Currency.CurrencyId = Account_Table.CurrencyId WHERE Account_Table.AccountId IN ('$accountidsend','$accountidrec')"
+            $thing = $db->query($sql)
+            $result = $thing->fetchArray();
+            $convertion = $result["Togbp"]
+            $result = $thing->fetchArray();
+            $convertion = $convertion / $result["Togbp"]
+            $convertedamount = $amount * $convertion
+            
             $sql = "SELECT Ballance From Account_Table WHERE AccountId = '$accountidsend'";
             $result = (($db->query($sql))->fetchArray());
-            if ($result - $ammount >= 0){
+            if ($result["Ballance"] - $ammount >= 0){
               $sql = "UPDATE Account_Table SET Ballance = Ballance - '$amount' Where AccountId = '$accountidsend'";
               $result = (($db->query($sql))->fetchArray());
               if ($result != FALSE){
-                $sql = "UPDATE Account_Table SET Ballance = Ballance + '$amount' Where AccountId = '$accountidrec'";
+                $sql = "UPDATE Account_Table SET Ballance = Ballance + '$convertedamount' Where AccountId = '$accountidrec'";
                 $result = (($db->query($sql))->fetchArray());
                 if($result != FALSE){
-                  $sql = "INSERT INTO Transaction_Table ";
+                  $time = time()
+                  $sql = "INSERT INTO Transaction_Table (Ammount,time,convertion,SenderId,ReciverId) Values('$amount','$time','$convertion','$accountidsend','$accountidrec')";
                 }
               }
             }
